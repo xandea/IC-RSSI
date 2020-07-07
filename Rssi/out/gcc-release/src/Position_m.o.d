c334295184414ed63c0d65c211b11c8ea6540df0
Alexandre <xandeebr@hotmail.com>
2020-07-07 18:28:58 -0300

    Realizando o calculos com 12 nos, formando um dodecagono regular

--- Rssi/out/gcc-release/src/Position_m.o.d
+++ Rssi/out/gcc-release/src/Position_m.o.d
@@ -1,49 +1,54 @@
 out/gcc-release//src/Position_m.o: src/Position_m.cc src/Position_m.h \
  /home/ale/omnetpp-5.6/include/omnetpp.h \
- ../inet/src/inet/common/INETDefs_m.h ../inet/src/inet/common/INETDefs.h \
- ../inet/src/inet/common/precompiled_release.h \
- ../inet/src/inet/common/precompiled.h ../inet/src/inet/opp_defines.h \
- ../inet/src/inet/features.h ../inet/src/inet/common/Compat.h \
- ../inet/src/inet/common/InitStages.h ../inet/src/inet/common/TagBase_m.h \
- ../inet/src/inet/common/Units_m.h ../inet/src/inet/common/Units.h \
- ../inet/src/inet/common/INETMath.h \
- ../inet/src/inet/common/geometry/Geometry_m.h \
- ../inet/src/inet/common/geometry/common/Coord.h \
- ../inet/src/inet/common/geometry/common/EulerAngles.h \
- ../inet/src/inet/common/geometry/common/Quaternion.h
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/INETDefs_m.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/INETDefs.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/precompiled_release.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/precompiled.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/opp_defines.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/features.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/Compat.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/InitStages.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/TagBase_m.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/Units_m.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/Units.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/INETMath.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/Geometry_m.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/common/Coord.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/common/EulerAngles.h \
+ /home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/common/Quaternion.h
 
 src/Position_m.h:
 
 /home/ale/omnetpp-5.6/include/omnetpp.h:
 
-../inet/src/inet/common/INETDefs_m.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/INETDefs_m.h:
 
-../inet/src/inet/common/INETDefs.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/INETDefs.h:
 
-../inet/src/inet/common/precompiled_release.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/precompiled_release.h:
 
-../inet/src/inet/common/precompiled.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/precompiled.h:
 
-../inet/src/inet/opp_defines.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/opp_defines.h:
 
-../inet/src/inet/features.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/features.h:
 
-../inet/src/inet/common/Compat.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/Compat.h:
 
-../inet/src/inet/common/InitStages.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/InitStages.h:
 
-../inet/src/inet/common/TagBase_m.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/TagBase_m.h:
 
-../inet/src/inet/common/Units_m.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/Units_m.h:
 
-../inet/src/inet/common/Units.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/Units.h:
 
-../inet/src/inet/common/INETMath.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/INETMath.h:
 
-../inet/src/inet/common/geometry/Geometry_m.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/Geometry_m.h:
 
-../inet/src/inet/common/geometry/common/Coord.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/common/Coord.h:
 
-../inet/src/inet/common/geometry/common/EulerAngles.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/common/EulerAngles.h:
 
-../inet/src/inet/common/geometry/common/Quaternion.h:
+/home/ale/omnetpp-5.6/samples/inet/src/inet/common/geometry/common/Quaternion.h:
